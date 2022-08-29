2363250c774d2b5428340885e68c73e5fb356b29
Gabriel Vieira <gabrielvieira1@protonmail.com>
2022-08-28 23:51:19 -0300

    Exemplos de buffer overflow

--- main.c
+++ main.c
@@ -2,7 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+void stack_overflow_01()
+{
+ // O uso de gets pode levar a graves vulnerabilidades de estouro de buffer. Use a alternativa segura fgets.
+ char buf[10];
+ memset(buf, 0, sizeof(buf));
+ printf("buf: ");
+
+ // gets(buf); Perigosa
+ fgets(buf, sizeof(buf), stdin); // Segura
+
+ printf("buf: %s\n", buf);
+ printf("strlen: %d\n", strlen(buf));
+}
+
+void stack_overflow_02()
 {
  // Stack corruption
  char buf2[16] = "overwriteme";
@@ -14,20 +28,36 @@ int main(int argc, char *argv[])
  printf("buf2 val: %s\n", buf2);
  printf("buf1 addr: %p\n", &buf1);
  printf("buf2 addr: %p\n", &buf2);
+}
 
+void heap_overflow_01()
+{
+ char *buf2 = malloc(10);
+ printf("buf2: ");
+
+ // gets(buf2); Perigosa
+ fgets(buf2, sizeof(buf2), stdin); // Segura
+
+ printf("buf2: %s\n", buf2);
+ printf("strlen: %d\n", strlen(buf2));
+ free(buf2);
+}
+
+void heap_overflow_02()
+{
  // Heap corruption
- char *buf3 = malloc(12 * sizeof(char)); //Fuga de memória. A memória alocada por malloc nunca é liberada. Na função principal
+ char *buf3 = malloc(12 * sizeof(char)); // Fuga de memória. A memória alocada por malloc nunca é liberada. Na função principal
  char *buf4 = malloc(12 * sizeof(char));
 
-//O resultado de malloc, que pode retornar fluxos nulos para o primeiro argumento de strcpy. 
-//Isso pode resultar em comportamento indefinido. Considere adicionar uma verificação de nulidade.
-/*
-char *buf3 = malloc(-1);
-if (!buf3 || !buf4) {
-  perror("Erro na alocação de memória");
-  return 1;
-}
-*/
+ // O resultado de malloc, que pode retornar fluxos nulos para o primeiro argumento de strcpy.
+ // Isso pode resultar em comportamento indefinido. Considere adicionar uma verificação de nulidade.
+ /*
+ char *buf3 = malloc(-1);
+ if (!buf3 || !buf4) {
+   perror("Erro na alocação de memória");
+   return 1;
+ }
+ */
  strcpy(buf4, "mywordshere");
  strcpy(buf3, "1234567890123456789012345678901234567890");
 
@@ -35,6 +65,15 @@ if (!buf3 || !buf4) {
  printf("buf4 val: %s\n", buf4);
  printf("buf3 addr: %p\n", &buf3);
  printf("buf4 addr: %p\n", &buf4);
+ // free(buf3);
+ // free(buf4);
+}
 
+int main(int argc, char *argv[])
+{
+ stack_overflow_01();
+ stack_overflow_02();
+ heap_overflow_01();
+ heap_overflow_02();
  return 0;
 }
